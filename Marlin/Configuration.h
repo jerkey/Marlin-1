ed0b02e8fb0f715eb0be5e327e82c2fdae054198
jerkey <jerkey@halo.nu>
2013-12-29 19:22:49 -0800

    hearth working, software endstops turned off

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -302,8 +302,8 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 #define Y_HOME_DIR -1
 #define Z_HOME_DIR -1
 
-#define min_software_endstops true // If true, axis won't move to coordinates less than HOME_POS.
-#define max_software_endstops true  // If true, axis won't move to coordinates greater than the defined lengths below.
+#define min_software_endstops false // If true, axis won't move to coordinates less than HOME_POS.
+#define max_software_endstops false  // If true, axis won't move to coordinates greater than the defined lengths below.
 
 //============================= Bed Auto Leveling ===========================
 
